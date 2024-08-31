4aa5db735d3081efda5181d76a5ee8867fec6cfc
Alryum <alryum@yandex.ru>
2024-08-31 21:12:17 +0500

    модель уникальных предметов

--- backend/resources/admin.py
+++ backend/resources/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Resource, UserResource
+from .models import Resource, UserResource, UniqueItem, UserUniqueItem
 
 
 @admin.register(Resource)
 class ResourceAdmin(admin.ModelAdmin):
     list_display = ('name', )
+
+
+@admin.register(UniqueItem)
+class UniqueItemAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- backend/resources/models.py
+++ backend/resources/models.py
@@ -13,3 +13,21 @@ class UserResource(models.Model):
     )
     resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
     quantity = models.PositiveBigIntegerField(verbose_name='Количество')
+
+
+class UniqueItem(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Название')
+    image = models.ImageField(upload_to='unique')
+
+
+class UserUniqueItem(models.Model):
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name='unique'
+    )
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(verbose_name='Количество')
+
+"""
+В дальнейшем можно добавить каждому предмету вес выпадения / ценности
+Для алгоритма дропа предметов из всего пула
+"""
