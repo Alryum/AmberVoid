51c2fb41264a35ff521062b169c368fd7d435a32
Alryum <alryum@yandex.ru>
2024-08-31 20:24:43 +0500

    Новые модели ресурсов

--- backend/resources/admin.py
+++ backend/resources/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Resource, UserResource
 
-# Register your models here.
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    list_display = ('name', )
--- backend/resources/models.py
+++ backend/resources/models.py
@@ -2,61 +2,14 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
-class RawResources(models.Model):
-    user = models.OneToOneField(
-        get_user_model(), on_delete=models.CASCADE, related_name='raw_resources'
+class Resource(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Название')
+    image = models.ImageField(upload_to='resources')
+
+
+class UserResource(models.Model):
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name='resources'
     )
-    stone = models.PositiveIntegerField(default=0)
-    iron = models.PositiveIntegerField(default=0)
-    copper = models.PositiveIntegerField(default=0)
-    quartz = models.PositiveIntegerField(default=0)
-    amber = models.PositiveIntegerField(default=0)
-
-    def remove_resource(self, resource_name, amount):
-        current_value = getattr(self, resource_name)
-        if current_value >= amount:
-            setattr(self, resource_name, current_value - amount)
-            self.save()
-        else:
-            raise ValueError("Not enough resources")
-
-
-class ProcessedMaterials(models.Model):
-    user = models.OneToOneField(
-        get_user_model(), on_delete=models.CASCADE, related_name='processed_materials'
-    )
-    iron_ingot = models.PositiveIntegerField(default=0)
-    copper_ingot = models.PositiveIntegerField(default=0)
-    quartz_ingot = models.PositiveIntegerField(default=0)
-    amber_ingot = models.PositiveIntegerField(default=0)
-
-    def add_resource(self, resource_name, amount):
-        current_value = getattr(self, resource_name)
-        setattr(self, resource_name, current_value + amount)
-        self.save()
-
-
-# class UniqueItem(models.Model):
-#     name = models.CharField(max_length=50, unique=True, verbose_name='Название')
-    # предмет должен давать какие-то преимущества, как вариант можно вьебать мультипликатор просто
-
-
-class ResourceManager:
-    def __init__(self, user):
-        self.user = user
-        self.raw_resources = user.raw_resources
-        self.processed_materials = user.processed_materials
-
-    def process_iron(self):
-        if self.raw_resources.iron >= 10:
-            self.raw_resources.remove_resource('iron', 10)
-            self.processed_materials.add_resource('iron_ingot', 1)
-        else:
-            raise ValueError("Not enough iron to process")
-
-    def process_copper(self):
-        if self.raw_resources.copper >= 10:
-            self.raw_resources.remove_resource('copper', 10)
-            self.processed_materials.add_resource('copper_ingot', 1)
-        else:
-            raise ValueError("Not enough copper to process")
+    resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(verbose_name='Количество')
